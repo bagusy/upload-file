740236b097932c13201b4b79c8f828c937d04f19
bagusy <42733293+bagusy@users.noreply.github.com>
2018-09-06 09:35:32 +0700

    Delete add_order.php

--- add_order.php
+++
@@ -1,6 +0,0 @@
-<form action="<?php echo base_url(); ?>index.php/produk/action_add" method="post" accept-charset="utf-8">
-	<input type="text" name="name" placeholder="masukkan">
-
-	<input type="submit" name="save">
-	<?=$this->city_model->provinceDropdown()?>
-</form>
